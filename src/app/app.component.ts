b3e98cec4d6c74e984779385e27efd1870090c0c
Matiur Rasel <matiur@deshuniversal.com>
2023-05-01 00:19:50 +0600

    changes-ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeExpenseManagerApp';
+  menuButtonStatus: boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MonthsComponent } from './months/months.component';
+import { MonthComponent } from './month/month.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideNavComponent,
+    MonthsComponent,
+    MonthComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
